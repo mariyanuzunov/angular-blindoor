8189b2993910f38852c18f15a6150b3fd02b23da
Mariyan Uzunov <themariyanuzunov@gmail.com>
2021-08-08 15:00:14 +0300

    add register form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,16 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-
+import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.pattern(
+            /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/
+          ),
+        ],
+      ],
+      credentials: this.fb.group({
+        password: ['', [Validators.required, Validators.minLength(5)]],
+        repeatPassword: ['', [Validators.required, Validators.minLength(5)]],
+      }),
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      phone: ['', [Validators.required, Validators.pattern(/\d{10}/)]],
+    });
   }
 
+  registerHandler(): void {
+    console.log(`>>> registerHandler called`);
+  }
 }
