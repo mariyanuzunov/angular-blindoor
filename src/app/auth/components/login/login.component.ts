85cabb9fbd38199642de79f0ff16f5d48f6654f6
Mariyan Uzunov <themariyanuzunov@gmail.com>
2021-08-08 15:06:18 +0300

    add login form

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.pattern(
+            /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/
+          ),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
   }
 
+  loginHandler(): void {
+    console.log(`>>> loginHandler called`);
+  }
 }
