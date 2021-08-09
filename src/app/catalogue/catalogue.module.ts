587f08b0551309eeae560a66755a18b424e37c6d
Mariyan Uzunov <themariyanuzunov@gmail.com>
2021-08-09 17:29:11 +0300

    add catalogue-page container

--- src/app/catalogue/catalogue.module.ts
+++ src/app/catalogue/catalogue.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CatalogueRoutingModule } from './catalogue-routing.module';
+import { CataloguePageComponent } from './containers/catalogue-page/catalogue-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CataloguePageComponent
+  ],
   imports: [
     CommonModule,
     CatalogueRoutingModule
