a74718d31d95b310138aaaba6594427f831748d0
Mariyan Uzunov <themariyanuzunov@gmail.com>
2021-08-11 18:09:42 +0300

    add item details page comp

--- src/app/catalogue/catalogue-routing.module.ts
+++ src/app/catalogue/catalogue-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CataloguePageComponent } from './containers/catalogue-page/catalogue-page.component';
+import { ItemDetailsPageComponent } from './containers/item-details-page/item-details-page.component';
 
 const routes: Routes = [
-  { path: 'catalogue', component: CataloguePageComponent },
+  {
+    path: 'catalogue',
+    children: [
+      { path: '', pathMatch: 'full', component: CataloguePageComponent },
+      { path: ':id', component: ItemDetailsPageComponent },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/catalogue/catalogue.module.ts
+++ src/app/catalogue/catalogue.module.ts
@@ -5,9 +5,10 @@ import { CatalogueRoutingModule } from './catalogue-routing.module';
 import { CataloguePageComponent } from './containers/catalogue-page/catalogue-page.component';
 import { ItemPreviewComponent } from './components/item-preview/item-preview.component';
 import { MaterialModule } from '../material.module';
+import { ItemDetailsPageComponent } from './containers/item-details-page/item-details-page.component';
 
 @NgModule({
-  declarations: [CataloguePageComponent, ItemPreviewComponent],
+  declarations: [CataloguePageComponent, ItemPreviewComponent, ItemDetailsPageComponent],
   imports: [CommonModule, CatalogueRoutingModule, MaterialModule],
 })
 export class CatalogueModule {}
